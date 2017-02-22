8a419f280ff058233d86fec8c2330af372b8a0c3
mw <mw@kevin>
2017-02-22 13:05:06 -0400

    Estructura

---
+++ plantillas/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Latest compiled and minified JavaScript -->
+
+<link rel="stylesheet" href="css/style.css">
+holaaaaaaaaaaaaaaaaaaaaaaaaaa
+<meta charset="UTF-8">
+</head>
+<body>
+
+</body>
+<footer>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/custom.js"> 
+
+</script>
+</footer>
+</html>
